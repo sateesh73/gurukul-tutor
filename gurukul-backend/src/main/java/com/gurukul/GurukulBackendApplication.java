e129e11279f9454883b7b966c1d248afc0e3fd3d
sateesh73 <sateeshvarma73@gmail.com>
2025-09-18 18:01:47 +0530

    app startup

---
+++ gurukul-backend/src/main/java/com/gurukul/GurukulBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gurukul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GurukulBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GurukulBackendApplication.class, args);
+	}
+
+}
---
+++ gurukul-backend/src/test/java/com/gurukul/GurukulBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gurukul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GurukulBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
